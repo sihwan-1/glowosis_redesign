fa4beb7243f6825db943b6a64c2231c881f1e14f
sihwan-1 <rlatlghks0917@gmail.com>
2025-08-26 13:50:38 +0900

    섹션 4 작업중

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ function Menu_1() {
 }
 Menu_1();
 
+function Select() {
+  $('.select').click(function() {
+    $('.select > ul').toggleClass('active')
+  });
+}
+Select();
+
 function Swiper_2() {
   var swiper = new Swiper(".swiper-2", {
     slidesPerView: 3,
@@ -18,3 +25,22 @@ function Swiper_2() {
   });
 }
 Swiper_2();
+
+function Swiper_3() {
+  var swiper = new Swiper(".swiper-3", {
+    slidesPerView: 1.5,       // 양옆이 보이도록
+    spaceBetween: 20,
+    centeredSlides: true,     // 가운데 정렬
+    loop: true,
+    speed: 1000,
+    autoplay: {
+      delay: 3000, // 다음 슬라이드로 넘어가기 전 대기 시간 (3초)
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: ".swiper-box-3 .swiper-button-next",
+      prevEl: ".swiper-box-3 .swiper-button-prev",
+    },
+  });
+}
+Swiper_3();
